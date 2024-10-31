c02308abc7e0e78098c6438be9bab5f0b56f3881
amitkumrydv <98074991+amitkumrydv@users.noreply.github.com>
2024-10-31 19:43:10 +0530

    added logs framwork

--- src/main/java/com/api/payload/AuthenticatePyload.java
+++ src/main/java/com/api/payload/AuthenticatePyload.java
@@ -1,14 +1,20 @@
 package com.api.payload;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.api.pojoClass.AuthenticationPojo;
 import com.api.utilities.ReadConfig;
 
 public class AuthenticatePyload {
 	
-	AuthenticationPojo  authenticationPojo;
+	private static final Logger logger = LoggerFactory.getLogger(AuthenticatePyload.class);
 	
 	
 	public AuthenticationPojo authenticatePyloadData() {
+		
+		logger.info("Set the userId and password in the Authentication payload ");
 		ReadConfig readConfig = new ReadConfig();
 		AuthenticationPojo  authenticationPojo = new AuthenticationPojo();
 		
--- src/main/java/com/api/test/AuthenticateTest.java
+++ src/main/java/com/api/test/AuthenticateTest.java
@@ -1,32 +1,52 @@
 package com.api.test;
 
-import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.testng.Assert.assertTrue;
 
 import org.testng.annotations.Test;
 
 import com.api.endpoints.AuthenticateEndPoint;
-
 import io.restassured.response.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AuthenticateTest {
+	
+	private static final Logger logger =  (Logger) LoggerFactory.getLogger(AuthenticateTest.class);
 
 	@Test
-	public void AuthenticateTestApi() {
-		
+	public void authenticateTestApi() {
+		logger.info("Start authenticateTestApi test");
 		
 
 		// @formatter:off
 		Response response = AuthenticateEndPoint.authenticateResponse();
 		Long responseTime = response.getTime();
-
+		  try {
 		response.then()
 				      // Verify that the response Content-Type is JSON
 				     .contentType("application/json; charset=UTF-8")
-				     .log().all();
+				     .log().ifError() // Log the response if there is an error
+	                 .assertThat()
+	                 .statusCode(200);
 		
 		
-		assertTrue(responseTime < 1500, "Response time is within acceptable range");
+	 //   assertTrue(responseTime < 1500, "Response time is within acceptable range");
+		
+		
+	    // Log the response body
+       logger.info("Response: " + response.getBody().asString());
+
+
+		    } catch (AssertionError assertionError) {
+	            logger.error("Assertion error: " + assertionError.getMessage());
+	            // Rethrow the assertion error to mark the test as failed
+	            throw assertionError;  // This will mark the test as failed
+	        } catch (Exception e) {
+	            logger.warn("An error occurred: " + e.getMessage());
+	            // Optionally rethrow this exception to fail the test as well
+	            throw new RuntimeException(e); // Mark as failed due to a non-assertion error
+	        }
+		
 
 	}
 
